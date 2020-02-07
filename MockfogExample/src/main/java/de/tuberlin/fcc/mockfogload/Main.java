48d72c57b817b229f4fb7c7c0c5c7ecca8799153
Jacob Jona Fahlenkamp <fahlenkamp.jacob@gmail.com>
2020-02-07 12:57:23 +0100

    Make application more flexible with regards to input. Read config from environment variable or input file.
    fall back to default file name if environment variable is given but does not exists.

--- MockfogExample/src/main/java/de/tuberlin/fcc/mockfogload/Main.java
+++ MockfogExample/src/main/java/de/tuberlin/fcc/mockfogload/Main.java
@@ -1,26 +1,54 @@
 package de.tuberlin.fcc.mockfogload;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.*;
-
-import static spark.Spark.post;
+import java.io.*;
 
 public abstract class Main {
 	public static Config config;
 
-	public static void main(String[] args) throws Exception {
-		Yaml yaml = new Yaml(new Constructor(Config.class));
-		InputStream inputStream = new FileInputStream(new File("config.yaml"));
-		config = (Config) yaml.load(inputStream);
+	private static boolean isYamlFilename(String s){
+		String[] x = s.split("\\.");
+		return x.length == 2 && x[1].equals(".yaml");
+	}
+
+	private static String ensureFile(String[] args) throws IOException {
+		final String FALLBACK = "config.yaml";;
+
+		if (args.length == 0) {
+			return FALLBACK;
+		}
+		String arg = args[0];
+
+		if (isYamlFilename(arg)) {
+			return arg;
+		}
+		arg = System.getenv().get(arg);
+
+		if (arg == null){
+			return FALLBACK;
+		}
+
+		if (isYamlFilename(arg)) {
+			return arg;
+		}
+		File file = File.createTempFile("config", "yaml");
+		new FileOutputStream(file).write(arg.getBytes());
+		file.deleteOnExit();
+		return file.getAbsolutePath();
+	}
+
+	private static void loadConfig(String[] args) throws IOException {
+		String filename = ensureFile(args);
+
+		Yaml yaml = new Yaml(new Constructor(de.tuberlin.fcc.mockfogload.Config.class));
+		InputStream inputStream = new FileInputStream(new File(filename));
+		config =  yaml.load(inputStream);
+	}
+
+	public static void main(String[] args) throws IOException {
+		loadConfig(args);
 
 		for (Config.SourceConfig sourceConfig:config.sources){
 			Source.create(sourceConfig);
